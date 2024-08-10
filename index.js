6ce74369093608373175497647577eeb1db3adc3
OB-Adams <obobadams@gmail.com>
2024-08-10 00:25:23 +0000

    Update commit

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ var cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public/assets"));
+app.use(express.static("public/dist/assets"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/public/assets/index.html");
+  res.sendFile(__dirname + "/public/dist/assets/index.html");
 });
 
 // your first API endpoint...
