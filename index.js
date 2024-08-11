e62cb5fdd8f02401b5d6bd93f9aed776e4eca61f
OB-Adams <obobadams@gmail.com>
2024-08-11 02:08:46 +0000

    Update commit

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-// index.js
-// where your node app starts
-
-// init project
 let express = require("express");
 let app = express();
 
 require('dotenv').config();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require("cors");
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+let cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 }));
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static(__dirname + "/public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
@@ -29,7 +20,6 @@ const handler = (req, res) => {
   const { date } = req.params;
 
   if (!date) {
-    // Handle empty date parameter: return current time
     const currentTimestamp = Date.now();
     return res.json({
       unix: currentTimestamp,
@@ -37,7 +27,6 @@ const handler = (req, res) => {
     });
   }
 
-  // Try parsing the date
   let parsedDate;
   if (!isNaN(date)) {
     // Handling numeric timestamps
@@ -60,7 +49,6 @@ const handler = (req, res) => {
 
 app.get("/api/:date?", handler);
 
-// Listen on port set in environment variable or default to 3000
 let listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
